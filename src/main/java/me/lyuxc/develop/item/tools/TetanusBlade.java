7ee0a4eac19030239bc2748b8fe5c4455e50159e
lyuxc-unknow <2675796669@qq.com>
2023-09-02 17:31:50 +0800

    改进 - 破伤风之刃效果

--- src/main/java/me/lyuxc/develop/item/tools/TetanusBlade.java
+++ src/main/java/me/lyuxc/develop/item/tools/TetanusBlade.java
@@ -1,8 +1,6 @@
 package me.lyuxc.develop.item.tools;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
+import me.lyuxc.develop.Star;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
@@ -11,18 +9,13 @@ import org.jetbrains.annotations.NotNull;
 
 public class TetanusBlade extends SwordItem {
     public TetanusBlade(Properties pProperties) {
-        super(Tiers.IRON, -2, 65535, pProperties);
+        super(Tiers.IRON, 0, 65535, pProperties);
     }
 
     @Override
     public boolean hurtEnemy(@NotNull ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
-        pTarget.addEffect(new MobEffectInstance(MobEffects.POISON, 5, 5));
-        pAttacker.setHealth(pAttacker.getHealth() + 5);
+        pTarget.setHealth((float) (pTarget.getHealth() - (pAttacker.getHealth() * 0.5)));
+        pAttacker.setHealth(pAttacker.getHealth() + Star.random.nextInt((int) (pTarget.getMaxHealth() * 0.5)));
         return true;
-    }
-
-    @Override
-    public @NotNull Component getDescription() {
-        return Component.literal("1234");
     }
 }
